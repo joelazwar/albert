944c736720ea3262392965085363341375b8011e
Joel Azwar <43277890+joelazwar@users.noreply.github.com>
2022-12-19 14:30:27 +0000

    styling and animation changes

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+
+$("#preview").animate({opacity:0,height: [ "toggle"]})
+$("main").animate({opacity:1},2500)
+
+
 $( "#logo" ).click(function() {
     $( "#nav" ).animate({
-      opacity: 0,
-      height: [ "toggle", "swing" ],
-    }, 2500, function() {
-        $("#nav").attr("hidden",true)
-        $("#preview").attr("hidden",false)
-        .animate({
-            opacity:1
+        opacity: 0,
+        height: [ "toggle", "swing" ],
+      }, 2500, function() {
+        $("#preview").animate({
+            opacity:1,
+            height: [ "toggle", "swing" ],
         },2500)
-    });
-  });
+      });
+  })
 
 $(".home-btn").click(()=>{
     $( "#preview" ).animate({
         opacity: 0,
         height: [ "toggle", "swing" ],
       }, 2500, function() {
-        $("#preview").attr("hidden",true)
-        $("#nav").attr("hidden",false)
+        $("#nav")
         .animate({
             opacity:1,
             height: [ "toggle", "swing" ],
